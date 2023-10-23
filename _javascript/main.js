9efd1d08b6f655b9b3a2c4799d4941917335bf12
nicolorenzo <nicolorenzo@live.com>
2023-10-23 19:46:05 -0300

    Bio e projetos

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,3 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('Hello Bulma!');
+  const accordion = document.querySelectorAll('ul.accordion li');
+
+  const initAccordion = (element) => {
+    element.addEventListener('click', toggleAccordion);
+
+    function toggleAccordion() {
+      if (element.classList.contains('active')) {
+        element.classList.remove('active');
+      } else {
+        accordion.forEach((element) => element.classList.remove('active'));
+        element.classList.add('active');
+      }
+    }
+  };
+  accordion.forEach(initAccordion);
 });
